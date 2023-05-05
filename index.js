e0d745e71256a2a0f758de44f3fa2dccb4f4c167
pratham704 <125141594+pratham704@users.noreply.github.com>
2023-05-05 21:59:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/gettrending', async(req, res) => {
 
 
 
-    const results = await abc.find({}).sort({ updatedAt: -1 });
+    const results = await abc.find({}).sort({ updatedAt: 1 });
     const response = results.map((doc) => {
         return {
             blogs: doc.blogs,
